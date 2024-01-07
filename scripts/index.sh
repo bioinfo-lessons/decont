3ed01afec75aa5d1cc5b251ee911e5f6742d9285
Pratul Negredo <pratulneg@gmail.com>
2024-01-07 18:09:24 +0100

    Index.sh
    This script is used to index the contaminants file once it is filtered and the small nuclear are deleted from the file

--- scripts/index.sh
+++ scripts/index.sh
@@ -4,5 +4,19 @@
 # The STAR command is provided for you. You should replace the parts surrounded
 # by "<>" and uncomment it.
 
-# STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
-# --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+file_input=$1
+directory_output=$2
+
+# Asegúrate de que directory_output tenga un valor válido.
+if [ -z "$directory_output" ]; then
+    echo "Error: La variable directory_output está vacía."
+    exit 1
+fi
+
+mkdir -p "$directory_output"
+
+ STAR --runThreadN 4 --runMode genomeGenerate --genomeDir "$directory_output" \
+ --genomeFastaFiles "$file_input" --genomeSAindexNbases 9
+
+
+
