b7f497b1c856faa7c18f277aadd85a3c67f90ab2
Pratul Negredo <pratulneg@gmail.com>
2024-01-07 18:16:46 +0100

    merge_fastqs.sh
    This script is used to merge the different .fastq files of the same sample.

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -3,3 +3,20 @@
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <input_directory> <output_directory> <sample_id>"
+    exit 1
+fi
+
+initial_directory=$1
+output_directory=$2
+sample_id=$3
+
+zcat "$initial_directory"/"$sample_id"*.fastq.gz > "$output_directory"/"$sample_id".fastq
+
+gzip "$output_directory"/"$sample_id".fastq
+
+
+
